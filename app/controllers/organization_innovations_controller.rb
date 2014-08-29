a868774a67e241c2d78642b1986b4e290a15f479
Chris Scott <cscott@problemsolutions.net>
2014-08-29 10:30:20 -0400

    fixes #10, controllers always return JSON. removed html routes from controllers and added error handling to single GET single.

--- app/controllers/organization_innovations_controller.rb
+++ app/controllers/organization_innovations_controller.rb
@@ -10,15 +10,11 @@ class OrganizationInnovationsController < ApplicationController
   # GET /organization_innovations/1
   # GET /organization_innovations/1.json
   def show
-  end
-
-  # GET /organization_innovations/new
-  def new
-    @organization_innovation = OrganizationInnovation.new
-  end
-
-  # GET /organization_innovations/1/edit
-  def edit
+    if @organization_innovation
+      render json: @organization_innovation.to_json(:include => :innovations)
+    else
+      render json: { error: "No organization:innovation with id: #{params[:id]}"}, status: 400
+    end
   end
 
   # POST /organization_innovations
@@ -26,28 +22,20 @@ class OrganizationInnovationsController < ApplicationController
   def create
     @organization_innovation = OrganizationInnovation.new(organization_innovation_params)
 
-    respond_to do |format|
-      if @organization_innovation.save
-        format.html { redirect_to @organization_innovation, notice: 'Organization innovation was successfully created.' }
-        format.json { render :show, status: :created, location: @organization_innovation }
-      else
-        format.html { render :new }
-        format.json { render json: @organization_innovation.errors, status: :unprocessable_entity }
-      end
+    if @organization_innovation.save
+      render :show, status: :created, location: @organization_innovation
+    else
+      render json: @organization_innovation.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /organization_innovations/1
   # PATCH/PUT /organization_innovations/1.json
   def update
-    respond_to do |format|
-      if @organization_innovation.update(organization_innovation_params)
-        format.html { redirect_to @organization_innovation, notice: 'Organization innovation was successfully updated.' }
-        format.json { render :show, status: :ok, location: @organization_innovation }
-      else
-        format.html { render :edit }
-        format.json { render json: @organization_innovation.errors, status: :unprocessable_entity }
-      end
+    if @organization_innovation.update(organization_innovation_params)
+      render :show, status: :ok, location: @organization_innovation
+    else
+      render json: @organization_innovation.errors, status: :unprocessable_entity
     end
   end
 
@@ -55,16 +43,13 @@ class OrganizationInnovationsController < ApplicationController
   # DELETE /organization_innovations/1.json
   def destroy
     @organization_innovation.destroy
-    respond_to do |format|
-      format.html { redirect_to organization_innovations_url, notice: 'Organization innovation was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_organization_innovation
-      @organization_innovation = OrganizationInnovation.find(params[:id])
+      @organization_innovation = OrganizationInnovation.find_by_id(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
